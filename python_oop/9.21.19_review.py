23f2b3c7ebf7f722a2e9681cd649d21d8259cd70
Roy Ge <rgecoder@gmail.com>
2019-09-24 21:20:31 -0700

    9.22 practice

--- python_oop/9.21.19_review.py
+++ python_oop/9.21.19_review.py
@@ -1,4 +1,21 @@
 class User:
-  name = "Anna"
+  def __init__(self,name,email):
+    self.name = name
+    self.email = email
+    self.logged = True
+  def login(self):
+    self.logged = True
+    print(self.name + ' is logged in.')
+    return self
+  def logout(self):
+    self.logged = False
+    print(self.name + ' is not logged in')
+    return self
 
-anna = User()
+  def show(self):
+    print(f"name is {self.name}, email at {self.email} ")
+    return self
+
+  
+user1 = User('Roy', 'roy@gmail.com')
+user1.show()
